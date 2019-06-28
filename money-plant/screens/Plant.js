c189d1dd40885b3aca6b79ef668c30d97e8edd2b
martinsuhendra <suhendra.martin90@gmail.com>
2019-06-28 18:24:41 +0700

    add detail and tooltip

--- money-plant/screens/Plant.js
+++ money-plant/screens/Plant.js
@@ -5,15 +5,19 @@ import {
   TouchableOpacity,
   FlatList,
   Image,
-  Dimensions
+  Dimensions,
+  Modal,
+  TextInput
 } from "react-native";
-import { Entypo } from "@expo/vector-icons";
+import { Tooltip, Text as ToolText } from "react-native-elements";
+import { Entypo, EvilIcons } from "@expo/vector-icons";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import * as Progress from "react-native-progress";
 
 const Plant = props => {
   const [touched, setTouched] = useState([]);
   const [progress, setProgress] = useState(0);
+  const [modalVisible, setModalVisible] = useState(false);
 
   return (
     <>
@@ -55,11 +59,20 @@ const Plant = props => {
               borderRadius: (Dimensions.get("window").width * 0.9) / 2
             }}
           />
-          <Progress.Bar progress={progress} width={200} style={{marginTop : 10}} borderColor="#fff" animated={true} color="#9dddd9" />
+          <Progress.Bar
+            progress={progress}
+            width={200}
+            style={{ marginTop: 10 }}
+            borderColor="#fff"
+            animated={true}
+            color="#9dddd9"
+          />
         </View>
         <TouchableOpacity
           style={styles.button}
-          onPress={() => setProgress(progress + 0.3)}
+          onPress={() => {
+            setModalVisible(true);
+          }}
         >
           <View
             style={{
@@ -109,6 +122,57 @@ const Plant = props => {
 
         <Text>{JSON.stringify(touched)}</Text>
       </View>
+      <Modal
+        animationType="fade"
+        transparent={false}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#587e5b"
+          }}
+        >
+          <Text
+            style={{
+              fontFamily: "PingFangHK-Light",
+              textAlign: "center",
+              color: "#fff",
+              paddingVertical: 10
+            }}
+          >
+            Input Amount :
+          </Text>
+          <TextInput
+            textAlign={"center"}
+            placeholderTextColor="rgba(255,255,255,0.7)"
+            id="password"
+            style={styles.input}
+          />
+          <Text style={styles.text}> or </Text>
+          <View style={{ flexDirection: "row", alignItems : "baseline", justifyContent: "space-around" }}>
+            <Text style={styles.text}> Use Recommendation Settings </Text>
+            <Tooltip
+              popover={
+                <ToolText>
+                  Use recommended settings to allow the system calculate 10% of
+                  your salary as your savings value
+                </ToolText>
+              }
+              height={100}
+              width={300}
+              backgroundColor="#ffd02c"
+            >
+              <EvilIcons name="question" size={20} style={{color : "white" }} />
+            </Tooltip>
+          </View>
+        </View>
+      </Modal>
     </>
   );
 };
@@ -138,6 +202,16 @@ const styles = {
     fontFamily: "PingFangHK-Medium",
     textAlign: "center",
     color: "#fff"
+  },
+  input: {
+    height: 40,
+    width: 300,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    backgroundColor: "rgba(255,255,255,0.2)",
+    color: "#fff",
+    borderRadius: 10,
+    fontSize: 20
   }
 };
 
